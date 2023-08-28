530edde5d7800577dc0ce16ad6b25b0299973429
Preston Lo <prestonloz97@gmail.com>
2023-08-28 10:23:00 +0000

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -28,9 +28,33 @@ export function activate(context: vscode.ExtensionContext) {
 		// Listen for selection
 		treeView.onDidChangeSelection(event => {
 			const selectedItems = event.selection as SymbolTreeItem[];
+			let editor = vscode.window.activeTextEditor;
+			
 			if (selectedItems.length > 0) {
+				
 				const selectedLabel = selectedItems[0].label;
+				const start = selectedItems[0].start as vscode.Position;
+				const end = selectedItems[0].end as vscode.Position;
+				const range:vscode.Range = new vscode.Range(start, end);
+				
 				vscode.window.showInformationMessage(`Selected tree item: ${selectedLabel}`);
+				const blueBackgroundDecorationType = vscode.window.createTextEditorDecorationType({
+					// the last element is the opacity
+					backgroundColor: 'rgba(154, 154, 156, 0.3)',
+					// this opacity appleis to the text instead of the background color
+					// opacity: '0',
+					// this can be seen on minimap, the small block colors
+					overviewRulerColor: '#02fa0f', // green
+					// to show the block color on the right
+					overviewRulerLane: vscode.OverviewRulerLane.Full,
+					// minimapColor: 'red' // this color will be used in the minimap
+				  });
+
+				const decoration = { range };
+				if(editor)
+				{
+					editor.setDecorations(blueBackgroundDecorationType, [decoration]);
+				}
 			}
 		});
 		
@@ -65,10 +89,9 @@ class MyTreeDataProvider implements vscode.TreeDataProvider<SymbolTreeItem> {
 			// for now, just make it to default.json first
 			JSONPath = getJSONPath(null);
 			let data = getJSONData(JSONPath);
-		
+			var ranges: vscode.Range[] = [];
 			var document = editor.document;
 			var text = document.getText();
-			var ranges: vscode.Range[] = [];
 			var symbolType = null;
 			// have to put 'any', else this variable will have type 'unknown'
 			// then later loop will have error
@@ -87,75 +110,50 @@ class MyTreeDataProvider implements vscode.TreeDataProvider<SymbolTreeItem> {
 				symbolType = key;
 				// show type, eg: function, macro, struct, etc
 				arr.push(new SymbolTreeItem(symbolType, vscode.TreeItemCollapsibleState.None));
-				// u can put 'as any' at behind to solve type is 'unknown' error
-				// value = {"a" : [1,2]}
-				// k = "a"
-				// value[k][0] = 1
-				// value[k][1] = 2
-				for (const k in value)
-				{
-					let regex = value[k][0];
-					let flag = value[k][1];
-					let match = null;
-					// a dynamic regex
-					let _regex = new RegExp(regex, flag);
-	
-					if(k.toLocaleLowerCase().includes("whole"))
-					{
-						while (match = _regex.exec(text)) {
-							const start = document.positionAt(match.index);
-							const end = document.positionAt(match.index + match[0].length);
-							const range = new vscode.Range(start, end);
-							// put these words into array
-							ranges.push(range);
 
-							const blueBackgroundDecorationType = vscode.window.createTextEditorDecorationType({
-								// the last element is the opacity
-								backgroundColor: 'rgba(154, 154, 156, 0.3)',
-								// this opacity appleis to the text instead of the background color
-								// opacity: '0',
-								// this can be seen on minimap, the small block colors
-								overviewRulerColor: '#02fa0f', // green
-								// to show the block color on the right
-								overviewRulerLane: vscode.OverviewRulerLane.Full,
-								// minimapColor: 'red' // this color will be used in the minimap
-							  });
+				let regex_whole = value.whole[0];
+				let flag_whole = value.whole[1];
+				let regex_name = value.name[0];
+				let flag_name = value.name[1];
+				let match = null;
+				// a dynamic regex
+				let _regex_whole = new RegExp(regex_whole, flag_whole);
+				let _regex_name = new RegExp(regex_name, flag_name);
 
-							const decoration = { range };
-							editor.setDecorations(blueBackgroundDecorationType, [decoration]);
-							
+				while (match = _regex_whole.exec(text)) {
+					const start = document.positionAt(match.index);
+					const end = document.positionAt(match.index + match[0].length);
+					const range:vscode.Range = new vscode.Range(start, end);
+					// put these words into array
+					// ranges.push(range);
 
-
-
-							// editor.selection = new vscode.Selection(start, end);
-							// editor.revealRange(range);
-							raw_matches.push(match);
-						}
-					}
+					// editor.selection = new vscode.Selection(start, end);
+					// editor.revealRange(range);
+					raw_matches.push(match);
 					
-					// only push to array if the JSON 'key' contains this word
-					// thus, only these keys will be shown on the tree list
-					if(k.toLocaleLowerCase().includes("name"))
+					for (let raw_match of raw_matches)
 					{
-						for (let raw_match of raw_matches)
+						// _regex = /function\s+([a-zA-Z_][a-zA-Z_0-9]*)\s*\([^)]*\)\s*\{/;
+						// let match = raw_match.toString().match(_regex);
+						let match = _regex_name.exec(raw_match.toString());
+						// put these words into array
+						if(match)
 						{
-							// _regex = /function\s+([a-zA-Z_][a-zA-Z_0-9]*)\s*\([^)]*\)\s*\{/;
-							// let match = raw_match.toString().match(_regex);
-							let match = _regex.exec(raw_match.toString());
-							// put these words into array
-							if(match)
-							{
-								// if you set breakpoint and investigate the value
-								// [1] holds the value of function name only
-								matches.push(match[1])
-							}
-							else
-							{
-								matches.push('anonymous');
-							}
-
-							arr.push(new SymbolTreeItem(``+match, vscode.TreeItemCollapsibleState.None, ranges));
+							// if you set breakpoint and investigate the value
+							// [1] holds the value of function name only
+							matches.push(match[1])
 						}
+						else
+						{
+							matches.push('anonymous');
+						}
+						
+						// push to array, this will show the list of symbols later
+						arr.push(new SymbolTreeItem(
+							``+match, 
+							vscode.TreeItemCollapsibleState.None, 
+							start,
+							end));
 					}
 				}
 			}
@@ -187,7 +185,8 @@ class SymbolTreeItem extends vscode.TreeItem {
 		public readonly label: string,
 		// whether they collapse or not, hold `CTRL`, hover over TreeItemCollapsibleState to see more
 		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-		public readonly range?: vscode.Range[],
+		public readonly start?: vscode.Position,
+		public readonly end?: vscode.Position,
 		public readonly id?: string,
 		public readonly command?: vscode.Command,
 		// public readonly iconPath: string | Uri | { light: string | Uri; dark: string | Uri } | ThemeIcon;
