b6caa7138a7609a32893d14a0fefc474d6306b4e
Preston Lo <prestonloz97@gmail.com>
2023-09-14 04:54:49 +0000

    add test

---
+++ src/test/mytest/test5.c
@@ -0,0 +1,27 @@
+#if defined(WORD)
+#if !defined
+#if ! defined
+#if ((defined(WORD)))
+#ifdef WORD
+#elif defined(WORD)
+#elif !defined(WORD)
+#elif ! defined(WORD)
+#elif defined WORD
+#if (defined WORD)
+
+
+    // "guard": {
+    //     "comment1": "",
+    //     "whole": [
+    //         "(#if|#elif)\\s*([^def|defined])([^\\n]+)\\n",
+    //         "g"
+    //     ],
+    //     "operation": "",
+    //     "opening": [
+    //         "",
+    //         ""
+    //     ],
+    //     "after": [
+    //         " ", "("
+    //     ],
+    //     "ignoreCommentedCode": true
