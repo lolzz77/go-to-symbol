64b0b11807cd7db66d9e95bcaa568ea01e714f04
Preston Lo <prestonloz97@gmail.com>
2023-09-15 10:42:25 +0000

    add test cases

--- src/test/mytest/test5.c
+++ src/test/mytest/test5.c
@@ -19,25 +19,72 @@ errno = run_test(5);
 #else
 #endif
 
-#ifdef TEST
-#ifdef TEST2
-#if defined(A) || defined(B)
-#define NUM   (2048)
-#else
-#define NUM   (1024)
+#if ME == 2
 #endif
-#define MAX   (512)
-#define NUM   (128)
-#define NUM   (64)
-#pragma DATA(ME, "RAM");
-int ME[MAX];
-#pragma DATA(ME2, "RAM");
-int ME2[MAX];
-#pragma DATA(ME3, "RAM");
-int ME3[MAX*2];
-#endif
-#endif /* PS_TYPE_NAND */
 
+
+/* Change to "#if 0" to use the second one */
+
+#ifdef TEST
+    #ifdef TEST2
+            #if defined(A) || defined(B)
+                #define NUM   (2048)
+            #else
+                #define NUM   (1024)
+            #endif
+        #define MAX   (512)
+        #define NUM   (128)
+        #define NUM   (64)
+        #pragma DATA(ME, "RAM");
+        int ME[MAX];
+        #pragma DATA(ME2, "RAM");
+        int ME2[MAX];
+        #pragma DATA(ME3, "RAM");
+        int ME3[MAX*2];
+    #elif ME
+        int x;
+    #elif defined ME3
+        int x;
+    #elif defined(ME5)
+        int x;
+    #elif !defined ME4
+        int x;
+    #elif !defined(ME6)
+        int x;
+    #elif (defined(ME7) && defined(ME8))
+        int x;
+    #elif defined(Z) && (X >= 3 )
+        int x;
+    #elif (defined(A) && !defined(B)) || defined(C) || defined(D)
+        int x;
+    #else
+        int me;
+    #endif
+#endif
+
+#if 1
+#endif
+
+#if 0
+#endif
+
+#if ( ( TEST == 1 ) || ( TEST2 == 1 ) )
+#endif
 
 #if ( defined(A) && defined(B) )
+#endif
 
+#if ((defined(NUM)) && (NUM > 0U))
+#endif
+
+#if defined(A) && !defined(B) && !defined(C)
+#endif
+
+#if !( (defined(A) && defined(B)) || defined(C) )
+#endif
+
+#if defined(A) && !defined(B) && !defined(C) || defined(D) || defined(E)
+#endif
+
+#if defined( A ) && ! defined( B )
+#endif
